c80b0d984dd20bdb757e5bc85b3d5518f72e64cb
Karen <karen2005yulieth@gmail.com>
2023-03-21 21:59:48 -0500

    Diseño de la pagina de inicio

--- src/Vista/VistaPrincipal.java
+++ src/Vista/VistaPrincipal.java
@@ -10,18 +10,18 @@ public class VistaPrincipal extends JFrame {
     //inicializando
     JButton  botonIniciar, botonCreditos, botonSalir, botonAdmin, botonOperador, botonCocina, botonRepartidor;
     JPanel panelInicio = new JPanel();
+    JPanel panelBlanco = new JPanel();
     JPanel panelModulos = new JPanel();
+    JLabel fondo = new JLabel();
     JLayeredPane contenedor=new JLayeredPane();
 
     public VistaPrincipal() {
         this.setTitle("Hot Dogs Palace");
         this.setExtendedState(MAXIMIZED_BOTH);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setResizable(false);
         this.setVisible(true);
+        this.setBackground(Color.white);
         panelInicio();
-        panelContenedor();
-
     }
 
 
@@ -30,49 +30,63 @@ public class VistaPrincipal extends JFrame {
         //Panel que tendrá las etiquetas y botones
         panelInicio.setLayout(null);
         panelInicio.setVisible(true);
-        panelInicio.setOpaque(true);
-        panelInicio.setBounds(300,100,200,300);
+        panelInicio.setOpaque(false);
+        panelInicio.setBounds(0,0,1500,800);
         Color colo1=new Color(255,245,238);
-        panelInicio.setBackground(colo1);
+        panelInicio.setBackground(Color.white);
+
+        panelBlanco.setLayout(null);
+        panelBlanco.setVisible(true);
+        panelBlanco.setOpaque(true);
+        panelBlanco.setBounds(0,0,1400,800);
+        panelBlanco.setBackground(Color.white);
+        this.add(panelBlanco);
         //this.add(panelInicio);
 
         //boton de inicio
 
         botonIniciar = new JButton("Iniciar Sesion");
-        botonIniciar.setBounds(600, 300, 300, 113);
-        ImageIcon img= new ImageIcon("src/Vista/INICIO.png");// se le pone icono a boton
+        botonIniciar.setBounds(600, 570, 230, 80);
+        ImageIcon img= new ImageIcon("src/Imagenes/INICIO.png");// se le pone icono a boton
         Icon i= new ImageIcon(img.getImage().getScaledInstance(botonIniciar.getWidth(), botonIniciar.getHeight(), Image.SCALE_DEFAULT));
         botonIniciar.setIcon(i);
         botonIniciar.setLayout(null);
         botonIniciar.setOpaque(true);
         botonIniciar.setBorderPainted(false);
-        ImageIcon imgJugar1= new ImageIcon("src/Vista/INICI.png");// se le pone icono a boton
+        ImageIcon imgJugar1= new ImageIcon("src/Imagenes/INICIO2.png");// se le pone icono a boton
         Icon iconIniciar= new ImageIcon(imgJugar1.getImage().getScaledInstance(botonIniciar.getWidth(), botonIniciar.getHeight(), Image.SCALE_DEFAULT));
         botonIniciar.setRolloverIcon(iconIniciar);
-        botonIniciar.setBackground(colo1);
+        botonIniciar.setBackground(Color.white);
 
         botonIniciar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 panelInicio.setVisible(false);
                 panelModulos();
+                panelBlanco.setVisible(true);
+                fondo.setVisible(false);
                 panelModulos.setVisible(true);
+                contenedor.add(botonAdmin,Integer.valueOf(3));
+                contenedor.add(botonOperador,Integer.valueOf(3));
+                contenedor.add(botonRepartidor,Integer.valueOf(3));
+                contenedor.add(botonCocina,Integer.valueOf(3));
+                contenedor.add(panelModulos,Integer.valueOf(4));
 
             }
         });
         panelInicio.add(botonIniciar);
 
         botonSalir = new JButton("Salir");
-        botonSalir.setBounds(600, 500, 300, 113);
+        botonSalir.setBounds(1000, 600, 200, 70);
         //botonReglas.setFont(new Font("BeaufortforLOL-Bold", Font.ITALIC, 12));
-        ImageIcon imgSalir3= new ImageIcon("src/Vista/SALIR.png");// se le pone icono a boton
+        ImageIcon imgSalir3= new ImageIcon("src/Imagenes/SALIR.png");// se le pone icono a boton
         Icon iconSalir= new ImageIcon(imgSalir3.getImage().getScaledInstance(botonSalir.getWidth(), botonSalir.getHeight(), Image.SCALE_DEFAULT));
         botonSalir.setIcon(iconSalir);
         botonSalir.setOpaque(false);
         botonSalir.setBorderPainted(false);
-        botonSalir.setBackground(Color.black);
+        botonSalir.setBackground(Color.white);
 
-        ImageIcon imgSalirD= new ImageIcon("src/Vista/SALIR2.png");// se le pone icono a boton
+        ImageIcon imgSalirD= new ImageIcon("src/Imagenes/SALIR2.png");// se le pone icono a boton
         Icon iconSalirD= new ImageIcon(imgSalirD.getImage().getScaledInstance(botonSalir.getWidth(), botonSalir.getHeight(), Image.SCALE_DEFAULT));
         botonSalir.setRolloverIcon(iconSalirD);
 
@@ -85,6 +99,7 @@ public class VistaPrincipal extends JFrame {
             }
         });
         panelInicio.add(botonSalir);
+        panelContenedor();
 
     }
 
@@ -92,24 +107,25 @@ public class VistaPrincipal extends JFrame {
 
         //Panel que tendrá las etiquetas y botones
         panelModulos.setLayout(null);
-        panelModulos.setOpaque(true);
-        panelModulos.setBounds(300,100,1000,300);
+        panelModulos.setOpaque(false);
+        panelModulos.setVisible(false);
+        panelModulos.setBounds(300,100,1400,500);
         Color colo1=new Color(255,245,238);
-        panelModulos.setBackground(colo1);
+        //panelModulos.setBackground();
         this.add(panelModulos);
 
         botonAdmin = new JButton("Aministrador");
         botonAdmin.setBounds(100, 300, 300, 300);
-        ImageIcon img= new ImageIcon("src/Vista/ADMIN.png");// se le pone icono a boton
+        ImageIcon img= new ImageIcon("src/Imagenes/ADMIN.png");// se le pone icono a boton
         Icon i= new ImageIcon(img.getImage().getScaledInstance(botonAdmin.getWidth(), botonAdmin.getHeight(), Image.SCALE_DEFAULT));
         botonAdmin.setIcon(i);
         botonAdmin.setLayout(null);
         botonAdmin.setOpaque(true);
         botonAdmin.setBorderPainted(false);
-        ImageIcon imgadmin= new ImageIcon("src/Vista/ADMIN.png");// se le pone icono a boton
+        ImageIcon imgadmin= new ImageIcon("src/Imagenes/ADMIN.png");// se le pone icono a boton
         Icon iconAdmin= new ImageIcon(imgadmin.getImage().getScaledInstance(botonAdmin.getWidth(), botonAdmin.getHeight(), Image.SCALE_DEFAULT));
         botonAdmin.setRolloverIcon(iconAdmin);
-        botonAdmin.setBackground(colo1);
+        botonAdmin.setBackground(Color.white);
 
         botonAdmin.addActionListener(new ActionListener() {
             @Override
@@ -124,16 +140,16 @@ public class VistaPrincipal extends JFrame {
 
         botonOperador = new JButton("Operador");
         botonOperador.setBounds(400, 300, 300, 300);
-        ImageIcon imgopera= new ImageIcon("src/Vista/OPERADOR.png");// se le pone icono a boton
+        ImageIcon imgopera= new ImageIcon("src/Imagenes/OPERADOR.png");// se le pone icono a boton
         Icon iOpera= new ImageIcon(imgopera.getImage().getScaledInstance(botonOperador.getWidth(), botonOperador.getHeight(), Image.SCALE_DEFAULT));
         botonOperador.setIcon(iOpera);
         botonOperador.setLayout(null);//
         botonOperador.setOpaque(true);
         botonOperador.setBorderPainted(false);
-        ImageIcon imgaopera= new ImageIcon("src/Vista/OPERADOR.png");// se le pone icono a boton
+        ImageIcon imgaopera= new ImageIcon("src/Imagenes/OPERADOR.png");// se le pone icono a boton
         Icon iconopera= new ImageIcon(imgaopera.getImage().getScaledInstance(botonOperador.getWidth(), botonOperador.getHeight(), Image.SCALE_DEFAULT));
         botonOperador.setRolloverIcon(iconopera);
-        botonOperador.setBackground(colo1);
+        botonOperador.setBackground(Color.white);
 
         botonOperador.addActionListener(new ActionListener() {
             @Override
@@ -148,16 +164,16 @@ public class VistaPrincipal extends JFrame {
 
         botonCocina = new JButton("Cocina");
         botonCocina.setBounds(700, 300, 300, 300);
-        ImageIcon imgococina= new ImageIcon("src/Vista/COCINA.png");// se le pone icono a boton
+        ImageIcon imgococina= new ImageIcon("src/Imagenes/COCINA.png");// se le pone icono a boton
         Icon iCocina= new ImageIcon(imgococina.getImage().getScaledInstance(botonCocina.getWidth(), botonCocina.getHeight(), Image.SCALE_DEFAULT));
         botonCocina.setIcon(iCocina);
         botonCocina.setLayout(null);
         botonCocina.setOpaque(true);
         botonCocina.setBorderPainted(false);
-        ImageIcon imgaCocina= new ImageIcon("InterfazProyecto/Vista/COCINA.png");// se le pone icono a boton
+        ImageIcon imgaCocina= new ImageIcon("src/Imagenes/COCINA.png");// se le pone icono a boton
         Icon iconoCocina= new ImageIcon(imgaCocina.getImage().getScaledInstance(botonCocina.getWidth(), botonCocina.getHeight(), Image.SCALE_DEFAULT));
         botonCocina.setRolloverIcon(iconoCocina);
-        botonCocina.setBackground(colo1);
+        botonCocina.setBackground(Color.white);
 
         botonCocina.addActionListener(new ActionListener() {
             @Override
@@ -175,16 +191,16 @@ public class VistaPrincipal extends JFrame {
 
         botonRepartidor = new JButton("Repartidor");
         botonRepartidor.setBounds(1000, 300, 300, 300);
-        ImageIcon imgorRepartidor= new ImageIcon("InterfazProyecto/Vista/REPARTIDOR.png");// se le pone icono a boton
+        ImageIcon imgorRepartidor= new ImageIcon("src/Imagenes/REPARTIDOR.png");// se le pone icono a boton
         Icon iRepartidor= new ImageIcon(imgorRepartidor.getImage().getScaledInstance(botonRepartidor.getWidth(), botonRepartidor.getHeight(), Image.SCALE_DEFAULT));
         botonRepartidor.setIcon(iRepartidor);
         botonRepartidor.setLayout(null);
         botonRepartidor.setOpaque(true);
         botonRepartidor.setBorderPainted(false);
-        ImageIcon imgRepartidor= new ImageIcon("InterfazProyecto/Vista/REPARTIDOR.png");// se le pone icono a boton
+        ImageIcon imgRepartidor= new ImageIcon("src/Imagenes/REPARTIDOR.png");// se le pone icono a boton
         Icon iconoRepartidor= new ImageIcon(imgRepartidor.getImage().getScaledInstance(botonRepartidor.getWidth(), botonRepartidor.getHeight(), Image.SCALE_DEFAULT));
         botonRepartidor.setRolloverIcon(iconoRepartidor);
-        botonRepartidor.setBackground(colo1);
+        botonRepartidor.setBackground(Color.white);
 
         botonRepartidor.addActionListener(new ActionListener() {
             @Override
@@ -198,19 +214,26 @@ public class VistaPrincipal extends JFrame {
         panelModulos.add(botonRepartidor);
     }
 
+
     public void panelContenedor(){
-        JLabel fondo = new JLabel();
-        ImageIcon imagen =(new ImageIcon("src/Imagenes/img.png"));
+        ImageIcon imagen =(new ImageIcon("img.png"));
         fondo.setIcon(imagen);
         fondo.setSize(imagen.getIconWidth(), imagen.getIconHeight());
+        fondo.setBounds(100,40,1400,600);
+        //fondo.setBounds(100,0,1000,70);
 
 
-        contenedor.add(fondo,JLayeredPane.DEFAULT_LAYER);
-        contenedor.add(fondo,JLayeredPane.PALETTE_LAYER);
-        contenedor.add(panelInicio,JLayeredPane.DRAG_LAYER);
+
+        contenedor.add(panelBlanco,Integer.valueOf(0));
+        contenedor.add(fondo,Integer.valueOf(1));
+        contenedor.add(panelInicio,Integer.valueOf(2));
+
+
 
         this.getContentPane().add(contenedor);
-        this.setSize(imagen.getIconWidth(), imagen.getIconHeight());
+        this.setSize(getMaximumSize());
+        //this.setSize(imagen.getIconWidth(), imagen.getIconHeight());
+        this.setExtendedState(MAXIMIZED_BOTH);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
